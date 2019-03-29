61178c3be47af0c55598f4c869d7b19eafd2de8c
Pure Krome <github@world-domination.com.au>
2019-03-29 16:42:15 +1100

    Update packages and README.md (#19)

--- src/content/Startup.cs
+++ src/content/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Swashbuckle.AspNetCore.Swagger;
 using System;
 
 namespace Homely.AspNetCore.WebApi.Template
